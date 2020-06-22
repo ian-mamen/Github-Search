ef55d43ad78f93da34f0c312386247cb7ee6e40d
Ian Amuga <amuga72@gmail.com>
2020-06-22 11:25:42 +0300

    added request service to user component

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,2 +1,29 @@
-export class Routing.Module {
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
+
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component'
+
+
+const routes:Routes=[
+    {path:"user",component:UserComponent},
+    {path:"about",component:AboutComponent},
+    {path:"",redirectTo:"/user",pathMatch:"full"},
+    //{path: 'user/:id', component: UserComponent},
+    {path:'**',component:NotFoundComponent}
+  ]
+
+  @NgModule({
+    imports: [
+      CommonModule,
+      RouterModule.forRoot(routes)
+    ],
+    exports:[RouterModule],
+    declarations: []
+  })
+
+export class Routing.module {
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../users.service';
+import {UserRequestService} from '../user-request.service';
+import {User} from '../user';
+import {Repository} from '../repository';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers:[UsersService,UserRequestService] 
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+  repository:Repository[];
+
+  userName=""
+  constructor(private usersService:UserRequestService) { 
+
+  }
+  submitUser(){
+    this.usersService.userRequest(this.userName)
+    this.usersService.repositoryrequest(this.userName)
+    
+}
 
   ngOnInit(): void {
+    this.usersService.userRequest('ian-mamen')
+    this.usersService.repositoryrequest('ian-mamen')
+         
+    this.user=this.usersService.user
+    this.repository=this.usersService.repository
+    console.log(this.repository)
   }
 
 }
