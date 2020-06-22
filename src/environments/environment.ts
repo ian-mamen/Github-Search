fedbf24af42fd433d9b6ca0e64f5cc154594b08d
Ian Amuga <amuga72@gmail.com>
2020-06-22 11:02:45 +0300

    added api key to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey: 'a3461a2e91d4ab939fd047869b2329da0ebcc47d'
 };
 
 /*
